073c5551e8b797f5dcbd6075ef269b41ac3a2852
Kelly Dang <xiaohandang@gmail.com>
2021-03-01 15:08:08 -0800

    Update cognito settings

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ca-central-1_n2A3eiF6P', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7erkq374b4o37vmch54sigj4pj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ca-central-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
